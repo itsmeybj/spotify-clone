ea88cd2a01420bbe4b407aecfd7ebf431b892b7f
itsmeybj <itsmeyj683@gmail.com>
2023-10-24 18:57:40 +0530

    api name changed

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import Cookies from "js-cookie"
 
-const url = "http://localhost:5000";
+//const url = "http://localhost:5000";
+const url = "https://spotify-clone-service.onrender.com";
 //login and register
 export const authUserApiDataToServer = async (route, data) => {
   console.log(data)
